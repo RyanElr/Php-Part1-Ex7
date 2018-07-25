30c67e24548474b0ba76e743ee7b9f65c042ba79
Ryan <ryan.elr@outlook.com>
2018-07-25 14:30:06 +0200

    ex7

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice7 Part1</title>
+  </head>
+  <body>
+    <?php
+    $name = 'El-rhichachmi';
+    $lastname = 'Ryan';
+    $age = 22;
+    echo ('Bonjour '.$name.' '.$lastname.', tu as '.$age.' ans.')
+    ?>
+  </body>
+</html>
